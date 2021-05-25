8a423062cf7829f5bab057ae439dfd9a33780d3c
luzip665 <test@test.de>
2021-05-25 17:52:34 +0200

    init

---
+++ launchpad/quickbuttons.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+PKG_NAME="exaile-plugin-quickbuttons"
+PKG_VERSION="0.1"
+DEB_VERSION="0ubuntu4"
+ARCH="all"
+
+TMP_DIR="/tmp/ex_build/"
+PKG_DIR="${PKG_NAME}_${PKG_VERSION}-${DEB_VERSION}_${ARCH}"
+VER_STRING="${PKG_VERSION}-${DEB_VERSION}"
+TIMESTAMP=`date -R` # Thu, 23 Sep 2010 21:36:01 +0200
+
+export DESTDIR=$TMP_DIR$PKG_DIR
+
+CHANGESFILE="${DESTDIR}.changes"
+
+rm -rf "${TMP_DIR}"
+mkdir -p $DESTDIR/debian
+
+cp -r ../exaile/plugins/quickbuttons/* $DESTDIR
+
+cp -r plugin_quickbuttons/* $DESTDIR/debian
+
+cd $DESTDIR
+
+sed -i "s/<#VERSION#>/$VER_STRING/g" debian/changelog
+sed -i "s/<#TIMESTAMP#>/$TIMESTAMP/g" debian/changelog
+
+echo 'override_dh_installdocs:' >> debian/rules
+
+## This happens on launchpad build server
+#dpkg-buildpackage
+#exit 0;
+##
+
+dpkg-source -b .
+dpkg-genchanges > $CHANGESFILE
+debsign -k Launchpad $CHANGESFILE
+dput ppa:luzip665/ppa $CHANGESFILE
