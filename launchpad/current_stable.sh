3296e3846713522a699c214b1cb8ce8844deecec
luzip665 <luzip665@posteo.de>
2022-01-25 23:21:38 +0100

    Correcting version in stable package

--- launchpad/current_stable.sh
+++ launchpad/current_stable.sh
@@ -2,10 +2,11 @@
 
 PKG_NAME="exaile"
 PKG_VERSION="4.1.1"
-DEB_VERSION="0ubuntu40"
+DEB_VERSION="1ubuntu0"
 ARCH="all"
-PPA="ppa"
-#PPA="exaile"
+#PPA="ppa:luzip665/ppa" # Testing
+#PPA="ppa:luzip665/exaile" # inofficial
+PPA="ppa:exaile-devel/ppa" # official
 
 TMP_DIR="/tmp/ex_build/"
 PKG_DIR="${PKG_NAME}_${PKG_VERSION}-${DEB_VERSION}_${ARCH}"
@@ -33,6 +34,9 @@ cd $DESTDIR
 sed -i "s/<#VERSION#>/$VER_STRING/g" debian/changelog
 sed -i "s/<#TIMESTAMP#>/$TIMESTAMP/g" debian/changelog
 
+#Set version in exaile
+sed -i "s|__version__ = \"devel\"|__version__ = \"$PKG_VERSION\"|" xl/version.py
+
 ## This happens on launchpad build server
 #dpkg-buildpackage
 #exit 0;
@@ -41,4 +45,4 @@ sed -i "s/<#TIMESTAMP#>/$TIMESTAMP/g" debian/changelog
 dpkg-source -b .
 dpkg-genchanges > $CHANGESFILE
 debsign -k Launchpad $CHANGESFILE
-dput ppa:luzip665/$PPA $CHANGESFILE
+dput $PPA $CHANGESFILE
