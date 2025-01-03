4259fee9a98d33b7914f3cb4150c2ae02f7208c1
luzip665 <luzip665@posteo.de>
2025-01-03 22:01:07 +0100

    launchpad package for 4.1.4-beta1

--- launchpad/current_stable.sh
+++ launchpad/current_stable.sh
@@ -2,8 +2,8 @@
 set -x
 
 PKG_NAME="exaile"
-EXAILE_VERSION="4.1.3"
-PKG_VERSION="4.1.3"
+EXAILE_VERSION="4.1.4-beta1"
+PKG_VERSION="4.1.4~beta1"
 DEB_VERSION="0ubuntu0"
 ARCH="all"
 #PPA="ppa:luzip665/ppa" # Testing
